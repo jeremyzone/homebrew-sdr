dbc5f38a0d4039e2d70cc5638bce0b76094a0bae
jeremyzone <jeremy.whiting@gmail.com>
2015-04-24 15:49:17 -0500

    Update gr-osmosdr.rb

--- gr-osmosdr.rb
+++ gr-osmosdr.rb
@@ -4,49 +4,17 @@ class GrOsmosdr < Formula
   homepage "http://sdr.osmocom.org/trac/wiki/GrOsmoSDR"
   head "git://git.osmocom.org/gr-osmosdr"
 
-  resource "Cheetah" do
-    url "https://pypi.python.org/packages/source/C/Cheetah/Cheetah-2.4.4.tar.gz"
-    sha1 "c218f5d8bc97b39497680f6be9b7bd093f696e89"
-  end
-
-  resource "lxml" do
-    url "https://pypi.python.org/packages/source/l/lxml/lxml-3.4.1.tar.gz"
-    sha1 "c09f4e8e71fc9d49fb43bf33821da816ce887396"
-  end
-  if build.without? "brewed-python"
-    resource "matplotlib" do
-      url "https://downloads.sourceforge.net/project/matplotlib/matplotlib/matplotlib-1.4.2/matplotlib-1.4.2.tar.gz"
-      sha1 "242c57ddae808b1869cad4b08bb0973c513e12f8"
-    end
-  end
-
   depends_on "cmake" => :build
   depends_on "gnuradio"
   depends_on "gr-iqbal"
   depends_on "librtlsdr"
-    if build.with? "brewed-python"
-    depends_on "matplotlib" => :python
-  end
 
   def install
-    ENV.prepend_create_path 'PYTHONPATH', libexec+'lib/python2.7/site-packages'
+     ENV.prepend_create_path 'PYTHONPATH', libexec+'lib/python2.7/site-packages'
     python_args = ["install", "--prefix=#{libexec}"]
     %w[Cheetah lxml].each do |r|
       resource(r).stage { system "python", "setup.py", *python_args }
     end
-    if build.without? "brewed-python"
-      resource("matplotlib").stage do
-        if MacOS.version >= :yosemite
-          inreplace "setupext.py", "'freetype2', 'ft2build.h',", "'freetype2', 'freetype2/ft2build.h',"
-        end
-        system "python", "setup.py", *python_args
-      end
-    end
-    python_fortran_args = ["build", "--fcompiler=gfortran", *python_args]
-    %w[numpy scipy].each do |r|
-      resource(r).stage { system "python", "setup.py", *python_fortran_args }
-    end
-    
     mkdir "build" do
       args = %W[
         -DPYTHON_LIBRARY='#{%x(python-config --prefix).chomp}/lib/libpython2.7.dylib'
